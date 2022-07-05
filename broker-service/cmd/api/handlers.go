cb186a07bb75bd5499ebc1e549738583974339c5
Mohamed Allam <meda3gm@gmail.com>
2022-07-05 12:04:43 +0100

    makefile working

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -11,8 +11,5 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
-	// out, _ := json.MarshalIndent(payload, "", "\t")
-	// w.Header().Set("content-Type", "application/json")
-	// w.WriteHeader(http.StatusAccepted)
-	// w.Write(out)
+
 }
